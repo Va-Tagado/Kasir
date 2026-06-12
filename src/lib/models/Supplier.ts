d5799314d59e62878c5deca9da7f07b77bbf2f26
Va-Tagado <vaizaazzahra186@gmail.com>
2026-06-13 00:10:03 +0700

    Create Supplier.ts

---
+++ src/lib/models/Supplier.ts
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+
+const SupplierSchema = new mongoose.Schema({
+  name: String,
+  contact: String,
+  address: String,
+}, { timestamps: true });
+
+export default mongoose.models.Supplier || mongoose.model('Supplier', SupplierSchema);
